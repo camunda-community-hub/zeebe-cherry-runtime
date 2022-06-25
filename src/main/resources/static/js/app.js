12679a1e2b39cfdab5b8300f0888517da3fba10c
Dave Paroulek <david.paroulek@camunda.com>
2022-06-25 12:29:33 -0400

    Add very basic index.mustache home page

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+    // Check for click events on the navbar burger icon
+    $(".navbar-burger").click(function() {
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        $(".navbar-burger").toggleClass("is-active");
+        $(".navbar-menu").toggleClass("is-active");
+
+    });
+
+    console.log("app.js loaded v2022-06-25");
+});
